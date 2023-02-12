e90293dd6a58bf1fea2ad06c53fc9572ad42d118
Julia Todorova <juliatodorova@C02FF6WGMD6M.localdomain>
2023-02-12 13:29:08 -0800

    Check for flush

--- Calculator.py
+++ Calculator.py
@@ -28,30 +28,42 @@ class Calculator:
     # 2 points per 15 made
     def calc_15(self, calc_job):
         add_to = 15
+        result = []
         for x in calc_job:
             result = [
                 seq for i in range(len(x), 0, -1)
                 for seq in itertools.combinations(x, i)
                 if sum(seq) == add_to]
-            return result
+        return result
+
+    # all matching suit in hand or all matching
+    def calc_flush(self, players):
+        hand = []
+        # get card objects and transform face cards
+        for x in players:
+            x.values()
+            for i in x.values():
+                cards = []
+                hand.append(cards)
+                for p in i:
+                    cards.append(p.get_suit())
+
+        flush_hands = []
+        for x in hand:
+            if len(set(x)) == 1:
+                flush_hands.append(x)
+        return flush_hands
 
     # 3+ consecutive numbers
     def calc_runs(self, cards):
         print(1)
 
-    # 1 point if in hand 2 points if on turn
-    def calc_jack_suit(self, cards):
-        print(1)
-
-    # all matching suit in hand or all matching
-    # suit in crib hand with matching flip card
-    def calc_flush(self, cards):
-
-        for card in cards:
-            print(card)
-
     # 2 points per pair
     def calc_pairs(self, cards):
+        print(1)
+
+    # 1 point if in hand 2 points if on turn
+    def calc_jack_suit(self, cards):
         print(1)
 
     def add_points(self):
